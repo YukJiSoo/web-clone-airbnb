0d37c1d9a76c039035feefeec4b7a8b134350bca
YukJiSoo <wltn3231@naver.com>
2019-10-15 23:13:54 +0900

    Feat: jwt 관리 util 모듈 구현

---
+++ api-server/util/jwt-manager.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY;
+
+export const tokenCreator = data =>
+    new Promise((resolve, reject) => {
+        jwt.sign(
+            data,
+            TOKEN_SECRET_KEY,
+            { expiresIn: '100d', issuer: 'boost-airbnb', subject: 'user' },
+            (err, token) => {
+                if (err) reject(err);
+                resolve(token);
+            },
+        );
+    });
+
+export const isTokenValid = token => {
+    try {
+        const decoded = jwt.verify(token, TOKEN_SECRET_KEY);
+        return decoded;
+    } catch (err) {
+        console.log(err);
+        return undefined;
+    }
+};
