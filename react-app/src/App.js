855280eeecdf3e350d814dd6d23b0ea00f0b7e92
YukJiSoo <wltn3231@naver.com>
2019-10-17 17:44:57 +0900

    Feat: App.js에 apollo client module >    적용
    authentication을 위해 header에 token추가.
    현재 로그인, 회원가입 페이지가 완성되지 않았으므로 임시 토큰으로 대체.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,55 @@
+// Import React modules
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Import Apoll client modules
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// Import environment variable manage module
+import dotenv from 'dotenv';
+
+// Import pages
 import SearchRoom from 'pages/SearchRoom';
 
+// Import components
 import GlobalStyle from 'components/GlobalStyle';
 
+// Set Apollo client information
+dotenv.config();
+
+const httpLink = createHttpLink({ uri: process.env.REACT_APP_API_URL });
+const authLink = setContext((_, { headers }) => {
+    // TODO
+    // get the authentication token from local storage if it exists
+    // const token = localStorage.getItem('token');
+    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwibmFtZSI6IuyCrOyngOyImCIsImlhdCI6MTU3MTMwMzg0OSwiZXhwIjoxNTc5OTQzODQ5LCJpc3MiOiJib29zdC1haXJibmIiLCJzdWIiOiJ1c2VyIn0.shnHbz6gdrtojuSf9u7XRXxD_q2PsDro9tmd60yl3g8`;
+    return {
+        headers: {
+            ...headers,
+            authorization: `Bearer ${token}`,
+        },
+    };
+});
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+// App
 const App = () => {
     return (
-        <Router>
-            <GlobalStyle />
-            <Switch>
-                <Route exact path="/" component={SearchRoom} />
-            </Switch>
-        </Router>
+        <ApolloProvider client={apolloClient}>
+            <Router>
+                <GlobalStyle />
+                <Switch>
+                    <Route exact path="/" component={SearchRoom} />
+                </Switch>
+            </Router>
+        </ApolloProvider>
     );
 };
 
