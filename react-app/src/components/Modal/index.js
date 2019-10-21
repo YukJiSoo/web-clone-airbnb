d1842d33d1f7183760e4b250a27ab2b78b5259d5
YukJiSoo <wltn3231@naver.com>
2019-10-21 15:18:51 +0900

    Feat: Modal component 구현 및 test 통과

---
+++ react-app/src/components/Modal/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as Style from './style';
+
+export default () => {
+    return <Style.Modal data-testid="modal"></Style.Modal>;
+};
---
+++ react-app/src/components/Modal/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Modal = styled.div`
+    && {
+        position: absolute;
+        top: 4rem;
+
+        padding: 2rem;
+
+        background-color: white;
+        border-radius: 0.5rem;
+
+        box-shadow: 0 0 2px 0 lightgray;
+    }
+`;
