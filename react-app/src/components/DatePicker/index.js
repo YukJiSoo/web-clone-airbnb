b02a47dcfbd85bd0ec67ceb58950e8780914dc37
YukJiSoo <wltn3231@naver.com>
2019-10-22 13:27:23 +0900

    Feat: DatePicker component 구현
    airbnb의 react-dates 모듈 사용.

---
+++ react-app/src/components/DatePicker/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DayPickerRangeController } from 'react-dates';
+
+import Style from './style';
+
+export default () => {
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [focusedInput, setFocusedInput] = useState('startDate');
+
+    const onDatesChange = ({ startDate, endDate }) => {
+        setStartDate(startDate);
+        setEndDate(endDate);
+    };
+
+    const onFocusChange = focusedInput => {
+        if (focusedInput === null) setFocusedInput('startDate');
+        else setFocusedInput(focusedInput);
+    };
+
+    return (
+        <Style>
+            <DayPickerRangeController
+                startDate={startDate}
+                endDate={endDate}
+                focusedInput={focusedInput}
+                onFocusChange={onFocusChange}
+                onDatesChange={onDatesChange}
+                hideKeyboardShortcutsPanel={true}
+                numberOfMonths={2}
+            />
+        </Style>
+    );
+};
