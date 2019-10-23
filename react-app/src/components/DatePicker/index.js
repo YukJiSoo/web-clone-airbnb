d9a8db4e0b554da7e2d6f9bd14116b2e7cf16577
YukJiSoo <wltn3231@naver.com>
2019-10-23 10:53:30 +0900

    Feat: DatePicker props로 locale 설정

--- react-app/src/components/DatePicker/index.js
+++ react-app/src/components/DatePicker/index.js
@@ -4,16 +4,21 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import { DayPickerRangeController } from 'react-dates';
 
+import moment from 'moment';
+import 'moment/min/locales';
+
 import Style from './style';
 
-export default ({ requestToServer, datePicked }) => {
+export default ({ requestToServer, datePicked, localeLanguage }) => {
+    moment.locale(localeLanguage);
+
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [focusedInput, setFocusedInput] = useState('startDate');
 
     const onDatesChange = ({ startDate, endDate }) => {
-        const startDateString = startDate.format('MM[월]DD[일]');
-        const endDateString = endDate ? endDate.format('MM[월]DD[일]') : '체크아웃';
+        const startDateString = startDate.format('M[월]D[일]');
+        const endDateString = endDate ? endDate.format('M[월]D[일]') : '체크아웃';
         datePicked(`${startDateString} - ${endDateString}`);
 
         setStartDate(startDate);
@@ -39,6 +44,7 @@ export default ({ requestToServer, datePicked }) => {
                 onDatesChange={onDatesChange}
                 hideKeyboardShortcutsPanel={true}
                 numberOfMonths={2}
+                isOutsideRange={day => day.isSameOrBefore()}
                 monthFormat="YYYY[년] MM[월]"
             />
             <button onClick={saveButtonHandler(startDate, endDate)}>저장</button>
--- react-app/src/containers/SearchBar/index.js
+++ react-app/src/containers/SearchBar/index.js
@@ -36,6 +36,7 @@ export default () => {
                         setIsClicked(false);
                     }}
                     datePicked={setButtonName}
+                    localeLanguage="ko"
                 />
             ),
         },
