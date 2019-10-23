eef49dc94a657af4061592bccf0da8802ddc3df5
YukJiSoo <wltn3231@naver.com>
2019-10-23 16:19:06 +0900

    Feat: reserveRoom mutation resolver 구현
    booking model이용.

--- api-server/resolvers/room.js
+++ api-server/resolvers/room.js
@@ -1,7 +1,7 @@
 import { AuthenticationError } from 'apollo-server';
 
 const rooms = async (_, args, context) => {
-    // if (!context.user) throw new AuthenticationError('Token is expired or do not exist');
+    if (!context.user) throw new AuthenticationError('Token is expired or do not exist');
     const { Room } = context.model;
     const { filterOptions } = args;
 
@@ -17,4 +17,22 @@ const rooms = async (_, args, context) => {
     }
 };
 
+const reserveRoom = async (_, args, context) => {
+    if (!context.user) throw new AuthenticationError('Token is expired or do not exist');
+
+    const {
+        user: { id },
+        model: { Booking },
+    } = context;
+    const { data } = args;
+
+    try {
+        const created = await Booking.registerNewBooking({ ...data, userId: id });
+        return { success: created };
+    } catch (error) {
+        throw error;
+    }
+};
+
 export const Query = { rooms };
+export const Mutation = { reserveRoom };
