31b0d656805c9a341870ca295d73b181c518ce30
YukJiSoo <wltn3231@naver.com>
2019-10-24 13:21:11 +0900

    Chore: SearchButton에 className 추가

--- react-app/src/containers/SearchBar/SearchButton.js
+++ react-app/src/containers/SearchBar/SearchButton.js
@@ -6,7 +6,10 @@ import { isMobile } from 'Constants';
 export default ({ name, modalBody, isClicked, setIsClicked, onClick }) => {
     return (
         <div>
-            <button className={isClicked ? 'isClicked' : ''} onClick={onClick(isClicked, setIsClicked)}>
+            <button
+                className={isClicked ? 'isClicked Search-Button' : 'Search-Button'}
+                onClick={onClick(isClicked, setIsClicked)}
+            >
                 {name}
             </button>
             {!isMobile() && isClicked && <Modal body={modalBody} />}
