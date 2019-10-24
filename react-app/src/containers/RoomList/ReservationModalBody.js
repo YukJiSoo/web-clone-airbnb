8f7c569acf8ebbea9d5b0ac9d8a9e179abe8dcb8
YukJiSoo <wltn3231@naver.com>
2019-10-24 15:56:02 +0900

    Feat: 날짜 조건과 예약하기 모달 연동
    선택한 경우와 안한 경우 구별해서 rendering

--- react-app/src/containers/RoomList/ReservationModalBody.js
+++ react-app/src/containers/RoomList/ReservationModalBody.js
@@ -4,46 +4,53 @@ import * as Style from './style';
 import { getNightValueFromDateString } from 'util/ConverDate';
 
 export default ({ room, searchOptions }) => {
-    // const { date } = searchOptions;
-    // dummy data
-    const date = { checkIn: '2019-09-10', checkOut: '2019-09-20' };
+    const { date, personnel } = searchOptions;
+
     const nights = date
         ? getNightValueFromDateString({ startDateString: date.checkIn, endDateString: date.checkOut })
         : 0;
+    const guest = personnel ? personnel.adult + personnel.children : 0;
 
     return (
         <Style.ReservationModalBody>
             <h2>{room.name}</h2>
             <hr></hr>
             <label htmlFor="date">날짜</label>
-            {!date && (
-                <input className="Text-Center" id="empty-date" type="text" value="날짜를 입력해주세요" readOnly />
-            )}
+            {!date && <input className="Text-Center" id="date" type="text" value="날짜를 입력해주세요" readOnly />}
             {date && (
-                <Style.DateWrapper>
+                <Style.DateWrapper id="date">
                     <input className="Date-Input" id="start-date" type="text" value={date.checkIn} readOnly />
                     <span className="Date-Arrow">></span>
                     <input className="Date-Input" id="end-date" type="text" value={date.checkOut} readOnly />
                 </Style.DateWrapper>
             )}
             <label htmlFor="personnel">인원</label>
-            <input id="personnel" type="text" value="게스트 1명, 유아 1명" readOnly />
+            {personnel ? (
+                <input
+                    id="personnel"
+                    type="text"
+                    value={`게스트 ${guest}명${personnel.infant === 0 ? '' : `, 유아 ${personnel.infant}명`}`}
+                    readOnly
+                />
+            ) : (
+                <input className="Text-Center" id="personnel" type="text" value="인원을 설정해주세요" readOnly />
+            )}
             <div className="Price-Wrapper">
-                {date ? (
+                {date && personnel ? (
                     <>
                         <span>
-                            ￦ {room.price}원 X {nights}박
+                            ￦ {room.price}원 X 게스트 {guest}명 X {nights}박
                         </span>
                         <span>
-                            <strong>￦ {room.price * nights}원</strong>
+                            <strong>￦ {room.price * guest * nights}원</strong>
                         </span>
                     </>
                 ) : (
-                    <div className="Text-Center">날짜를 입력해주세요</div>
+                    <div className="Text-Center">숙박조건을 모두 입력해주세요</div>
                 )}
             </div>
             <hr></hr>
-            <button className={!date ? 'Non-Clickable' : ''}>예약하기</button>
+            <button className={!(date && personnel) ? 'Non-Clickable' : ''}>예약하기</button>
             <small>예약 확정 전에는 요금이 청구되지 않습니다</small>
         </Style.ReservationModalBody>
     );
