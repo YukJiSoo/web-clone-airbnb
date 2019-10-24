2179a4fdfda605f924764e83eb870fdc591a5251
YukJiSoo <wltn3231@naver.com>
2019-10-24 21:07:57 +0900

    Feat: apollo client error 처리 로직 추가

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 // Import environment variable manage module
@@ -35,8 +36,15 @@ const authLink = setContext((_, { headers }) => {
         },
     };
 });
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+        );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 const apolloClient = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: errorLink.concat(authLink.concat(httpLink)),
     cache: new InMemoryCache(),
 });
 
